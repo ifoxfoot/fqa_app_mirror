5abb9de52639c7b9539961bb0f4d470d5a92ed7a
ifoxfoot <ifoxfoot@gmail.com>
2022-08-04 13:27:05 -0700

    got everything integrated, but uploading files still non-functional

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(fqacalc)
 library(tidyverse)
+library(shinyglide)
 library(DT)
 
 #define table for data entered manually
@@ -14,12 +15,23 @@ ui <- fluidPage(
 
     #tab panel 1
     tabPanel("Calculate FQA Metrics",
+
+             #allow glide to be used in this tab
+             glide(
+               #labels for glide buttons
+               next_label = "Calculate FQA Metrics",
+               previous_label = "Go Back to Data Entry",
+
+               screen(
+
              fluidRow(
                sidebarPanel(
+                 titlePanel("Enter Data"),
 
                  #input regional data base
                  selectInput("db", label = "Select Regional FQAI Database",
-                          choices = fqacalc::db_names()),
+                          choices = fqacalc::db_names(),
+                          selected = "michigan_2014"),
 
                  #input data entry method
                  radioButtons("method", label = "Select Data Entry Method",
@@ -31,22 +43,22 @@ ui <- fluidPage(
 
                    condition = "input.method == 'upload'",
 
-                   #input file upload
+                   #input file upload widget
                    fileInput("uploaded_file", NULL, buttonLabel = "Upload...", multiple = F),
 
-                   #button to delete uploaded file
+                   #input button to delete uploaded file
                    actionButton("delete_upload", "Delete Uploaded File")
 
                    ), #conditional 1 parenthesis
 
 
-                #when data entry method is not upload, enter manually
+                #when data entry method is enter, allow user to enter data manually
                 conditionalPanel(
 
-                  condition = "input.method != 'upload'",
+                  condition = "input.method == 'enter'",
 
                   #input latin name
-                  uiOutput("latin_names"),
+                  uiOutput("latin_name"),
 
                   #input add species button
                   actionButton("add_species", "Add Species"),
@@ -59,24 +71,52 @@ ui <- fluidPage(
 
                 ), #conditional 2 parenthesis
 
-              ),#side panel parenthesis
+               ), #side bar panel
 
             mainPanel(
 
               #when user uploads file, show uploaded table
-              conditionalPanel("input.method = 'upload' && input.uploaded_file != 0",
+              conditionalPanel("input.method == 'upload' && input.uploaded_file != 0",
                                DTOutput("DT_upload")),
 
 
               #when user enters species manually, show what they enter
-              conditionalPanel("input.method != 'upload' && input.add_species != 0",
+              conditionalPanel("input.method == 'enter'",
                                DTOutput("DT_manual")),#conditional panel parenthesis
 
               )#main panel parenthesis
 
-            )#fluid row parenthesis
+             )#fluid row parenthesis
 
-           ),#tab panel 1 parenthesis
+            ),#screen 1 parenthesis
+
+            screen(
+              fluidRow(
+
+                conditionalPanel(
+                  condition = "input.method == 'upload'",
+                  #output table of metrics
+                  tableOutput("DT_all_upload"),
+                  #output
+                  plotOutput("c_hist_upload")
+                  ),#conditional 1 parenthesis
+
+                conditionalPanel(
+                  condition = "input.method == 'enter'",
+                  #output table of metrics
+                  tableOutput("DT_metrics_manual"),
+                  #output
+                  plotOutput("c_hist_manual")
+                )#conditional 2 parenthesis
+
+
+                )#fuildRow parenthesis
+
+              )#screen 2 parenthesis
+
+            )#glide parenthesis
+
+          ),#tab panel 1 parenthesis
 
     #tab panel 2
     tabPanel("Caclulate FQA Transect Metrics",
@@ -98,7 +138,7 @@ server <- function(input, output, session) {
   #creating reactive upload
   file_upload <- reactive({
     #require that a file be uploaded
-    req(input$uploaded_file | input$delete_upload)
+    req(input$uploaded_file)
     #getting extension
     ext <- tools::file_ext(input$uploaded_file$name)
     #reading in differently based on extension
@@ -109,16 +149,46 @@ server <- function(input, output, session) {
     })#file upload reactive parenthesis
 
   #render output table from uploaded file
-  output$DT_upload <- renderDT(file_upload())
+  output$DT_upload <- renderDT(
+    datatable(file_upload(),
+              selection = 'single',
+              options = list(autoWidth = FALSE,
+                             scrollX = TRUE,
+                             dom = 't')
+  ))
+
+  # #when delete all is clicked, clear all entries
+  # observeEvent(input$delete_upload, {
+  #   #make an empty df
+  #   file_upload() <- NULL
+  # })
+
+  # #metrics table output on FQA page
+  # output$DT_metrics_upload <- renderTable({
+  #   fqacalc::all_metrics(x = file_upload(), db = input$db)
+  # })
+  #
+  # #ggplot output
+  # output$c_hist_upload <- renderPlot({
+  #   ggplot(data = unique(file_upload()),
+  #          aes(x = file_upload()$c,
+  #              fill = file_upload()$native)) +
+  #     geom_histogram(bins = 11) +
+  #     labs(title = "Conservation Coefficient Histogram",
+  #          x = "Conservation Coefficient Score",
+  #          fill = "Native or Exotic") +
+  #     theme_classic()
+  # })
+
 
 #ENTER SPECIES MANUALLY
 #-------------------------------------------------------------------------------
   #species drop-down list based on region
-  output$latin_names <- renderUI({
+  output$latin_name <- renderUI({
     #create list of latin names based on regional list selected
     latin_names <- c("", unique(fqacalc::view_db(input$db)$scientific_name))
     #create a dropdown option
-    selectizeInput("latin_names", "Select Species", latin_names, selected = "")
+    selectizeInput("species", "Select Species", latin_names, selected = NULL)
     })# latin names parenthesis
 
   #create an object with no values to store inputs
@@ -128,7 +198,7 @@ server <- function(input, output, session) {
   observeEvent(input$add_species, {
     #find species
     new_entry <- data.frame(fqacalc::view_db(input$db) %>%
-                              dplyr::filter(scientific_name == input$latin_names))
+                              dplyr::filter(scientific_name == input$species))
     #bind new entry to table
     new_table = rbind(new_entry, data_entered())
     #print table
@@ -149,10 +219,39 @@ server <- function(input, output, session) {
     data_entered(t)
     })
 
-  #render output table from manually entered species
+  #when delete all is clicked, clear all entries
+  observeEvent(input$delete_manual_entries, {
+    #make an empty df
+    empty_df <- data.frame(row.names = names(fqacalc::crooked_island))
+    #assign it to the reactive value
+    data_entered(empty_df)
+  })
+
+  #render output table from manually entered species on data entry page
   output$DT_manual <- renderDT({
-    datatable(data_entered(), selection = 'single', options = list(dom = 't'))
-    })
+    datatable(data_entered(),
+              selection = 'single',
+              options = list(autoWidth = FALSE,
+                             scrollX = TRUE,
+                             dom = 't'))
+  })
+
+  #metrics table output on FQA page
+  output$DT_metrics_manual <- renderTable({
+    fqacalc::all_metrics(x = data_entered(), db = input$db)
+  })
+
+  #ggplot output
+  output$c_hist_manual <- renderPlot({
+    ggplot(data = unique(data_entered()),
+           aes(x = data_entered()$c,
+               fill = data_entered()$native)) +
+      geom_histogram(bins = 11) +
+      labs(title = "Conservation Coefficient Histogram",
+           x = "Conservation Coefficient Score",
+           fill = "Native or Exotic") +
+      theme_classic()
+  })
 
   }#server brackets
 
