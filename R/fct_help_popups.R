33a64f17d11673e4c6e9d46171263c3080488c83
ifoxfoot <ifoxfoot@gmail.com>
2023-05-23 10:21:04 -0700

    fixed error with popup

--- R/fct_help_popups.R
+++ R/fct_help_popups.R
@@ -7,30 +7,30 @@
 #' @noRd
 inventory_help <- function(){
   alert <- shinyalert::shinyalert(title = "Inventory Instructions", html = T, className = "help",
-                      text = tagList(strong("Step 1."),
-                                     "Select your regional FQA of interest. If you are unsure
+                                  text = tagList(strong("Step 1."),
+                                                 "Select your regional FQA of interest. If you are unsure
                             about which regional FQA database to use, consult the map on the
                             'About FQA' tab.",
-                                     br(),
-                                     br(),
-                                     strong("Step 2."),
-                                     "Decide if you would like to enter observations manually
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 2."),
+                                                 "Decide if you would like to enter observations manually
                             or if you would like to upload a file.",
-                                     br(),
-                                     br(),
-                                     strong("Step 3."),
-                                     "To upload a file, select a file from your device. The
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 3."),
+                                                 "To upload a file, select a file from your device. The
                             file must be a .csv, .tsv, or .xlsx file. The file must
-                            contain a column containing scientific names", strong(or), "acronyms.
+                            contain a column containing scientific names", strong("or"), "acronyms.
                             The file must be formatted such that the columns extend to
                             the top of the spreadsheet, with row 1 containing column
                             names. Select the name of the column that contains scientific
                             names or acronyms. You may delete the file by clicking
                             'Delete Uploaded File'.",
-                                     br(),
-                                     br(),
-                                     strong("Step 4."),
-                                     "To enter data manually, click on the
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 4."),
+                                                 "To enter data manually, click on the
                                      field that says 'Select Species', then start
                                      typing in the Genus . As you type a pick list
                                      will appear. Click on the species you want,
@@ -46,22 +46,21 @@ inventory_help <- function(){
                                      Regional FQA Databases'  tab. Note that some
                                      regional FQA databases are not comprehensive
                                      and may not contain every species in the region.",
-                                     br(),
-                                     "NOTE: Changing the regional database will automatically
+                                                 br(),
+                                                 "NOTE: Changing the regional database will automatically
                             clear your entries.",
-                                     br(),
-                                     br(),
-                                     strong("Step 5."),
-                                     "Click `Calculate FQA Metrics` button, located
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 5."),
+                                                 "Click `Calculate FQA Metrics` button, located
                                      in the upper right. You may come back to the
                                      data entry page to enter more data. You may
                                      also download a report. To learn more about
                                      the metrics calculated, visit the 'Equations tab.",
-                                     br(),
-                                     br(),
-                                     "To report a mistake or unexpected issue on this webpage,
-                            contact - ecomodteam@usace.army.mil"
-                      ))
+                                                 br(),
+                                                 br(),
+                                                 "To report a mistake or unexpected issue on this webpage,
+                                     contact - ecomodteam@usace.army.mil"))
 
   return(alert)
 }
@@ -75,29 +74,29 @@ inventory_help <- function(){
 #' @noRd
 cover_help <- function(){
   alert <- shinyalert::shinyalert(title = "Cover Weighted FQA Instructions", html = T, className = "help",
-                      text = tagList(strong("Step 1."),
-                                     "Select your regional FQA database of interest. If you are unsure
+                                  text = tagList(strong("Step 1."),
+                                                 "Select your regional FQA database of interest. If you are unsure
                                      about which regional FQA to use, consult the map on the
                                      'About FQA' tab.",
-                                     br(),
-                                     br(),
-                                     strong("Step 2."),
-                                     "Enter your cover method (e.g., method used to
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 2."),
+                                                 "Enter your cover method (e.g., method used to
                                      estimate abundance of species in plots). Percent
                                      Cover is the default (ranging from 1-100%).
                                      Other methods use percent ranges
                                      (e.g., cover categories), and then the midpoint
                                      of those ranges is used. For more information,
                                      see the 'More' tab.",
-                                     br(),
-                                     br(),
-                                     strong("Step 3."),
-                                     "Decide if you would like to enter observations manually
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 3."),
+                                                 "Decide if you would like to enter observations manually
                                      or if you would like to upload a file.",
-                                     br(),
-                                     br(),
-                                     strong("Step 4."),
-                                     "To upload a file, select a file from your
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 4."),
+                                                 "To upload a file, select a file from your
                                      device. The file must be a .csv, .tsv, or
                                      .xlsx file. The file must contain a column
                                      containing scientific names or acronyms, a
@@ -113,10 +112,10 @@ cover_help <- function(){
                                      the column with plot IDs to generate a plot
                                      summary. You may delete the file by clicking
                                      'Delete Uploaded File'.",
-                                     br(),
-                                     br(),
-                                     strong("Step 4."),
-                                     "To enter data manually, enter a transect ID
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 4."),
+                                                 "To enter data manually, enter a transect ID
                                      (you may only enter data for one transect at a time).
                                      Then enter a plot ID. If you intend to calculate
                                      metrics for a single plot, keep the same plot
@@ -137,22 +136,22 @@ cover_help <- function(){
                                      tab. Note that some regional FQA databases are
                                      not comprehensive and may not contain every
                                      species in the region.",
-                                     br(),
-                                     br(),
-                                     "NOTE: Changing the regional database or cover method
+                                                 br(),
+                                                 br(),
+                                                 "NOTE: Changing the regional database or cover method
                                      will automatically clear your entries.",
-                                     br(),
-                                     br(),
-                                     strong("Step 5."),
-                                     "To calculate metrics click `Calculate FQA
+                                                 br(),
+                                                 br(),
+                                                 strong("Step 5."),
+                                                 "To calculate metrics click `Calculate FQA
                                      Metrics` located upper right. You may come
                                      back to the data entry page to enter more
                                      data. You may also download a report. To learn
                                      more about the metrics calculated, visit the
                                      'Equations ' tab.",
-                                     br(),
-                                     br(),
-                                     "To report a mistake or unexpected issue on this webpage,
+                                                 br(),
+                                                 br(),
+                                                 "To report a mistake or unexpected issue on this webpage,
                                      contact – ecomodteam@usace.army.mil"))
   return(alert)
 }
